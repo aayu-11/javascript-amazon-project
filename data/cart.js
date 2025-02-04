5dbafe8b04177f49bbd5d54fcae4d79bacc24493
aayu-11 <ayushkumar.ak2409@gmail.com>
2025-02-04 17:56:55 +0530

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -105,8 +105,21 @@ function updateDeliveryOption(productId, deliveryOptionId) {
   saveToLocalStorage();
 }
 
+function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
+}
+
 export {
   cart,
+  loadCart,
   addToCart,
   removeFromCart,
   saveToLocalStorage,
